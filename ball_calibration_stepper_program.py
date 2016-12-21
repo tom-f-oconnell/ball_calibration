19e87d656974ce8737dbed8a99fd203d5a15e9ff
Tom O'Connell <toconnel@caltech.edu>
2016-12-20 22:34:41 -0800

    Added more checks on quality of data coming out. It seems many lines are malformed. Maybe go to a lower rate?

--- ball_calibration_stepper_program.py
+++ ball_calibration_stepper_program.py
@@ -21,6 +21,7 @@ import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 use_sensor = True
 
@@ -43,12 +44,21 @@ class OpticFlowSensor(serial.Serial):
         dataList = []
         while self.inWaiting() > 0:
             rawData = self.readline()
-            rawData = rawData.strip()
-            rawData = rawData.split(',')
+            # TODO just have the arduino output in a binary format
+            without_whitespace = rawData.strip()
+            without_commas = without_whitespace.split(',')
+
+            integerized = []
             try:
-                dataList.append([int(x) for x in rawData])
+                integerized = [int(x) for x in without_commas]
+                dataList.append(integerized)
             except:
                 pass
+            
+            # TODO remove. for debugging.
+            if len(integerized) != 4:
+                print('bad line: ' + rawData)
+
         return dataList
 
 def test_velocity(stepper, sensor, velocity):
@@ -72,7 +82,6 @@ def test_velocity(stepper, sensor, velocity):
 
     stepper.setVelocityLimit(stepper_id, steps_per_sec)
     
-    # does making it negative work?
     stepper.setTargetPosition(stepper_id, \
         int(np.sign(velocity)) * int(desiredNumMicrosteps))
     
@@ -89,14 +98,25 @@ def test_velocity(stepper, sensor, velocity):
     # TODO any way to know arrival time of these? how did ROS add 
     # timestamp?
     sensor_data = []
+    min_so_far = 4
     while time.time() < end:
         # something neater?
         if use_sensor:
-            sensor_data.append(sensor.readData())
+            new = sensor.readData()
+            sensor_data.append(new)
+
+            for n in new:
+                if len(n) < min_so_far:
+                    min_so_far = len(n)
+
+    print("smallest number of datapoints from any one read" + \
+        str(min_so_far))
 
     # TODO working? other code didn't seem to use this
-    if use_sensor:
-        sensor.stop()
+    # SENSOR SEEMS TO REPORT A SATURATED VALUE AFTER STOP?
+    # (not doing this) will just use empty reads in between trials
+    #if use_sensor:
+    #    sensor.stop()
 
     # I want the sensor to think it is at this position again
     # documentation unclear on effects
@@ -205,22 +225,18 @@ try:
     should_plot = True
 
     if use_sensor:
-        port = '/dev/ttyACM0'
+        port = '/dev/ttyACM1'
         sensor = OpticFlowSensor(port)
     else:
         sensor = None
     
     stepper_id = 0
 
-    print("Set the current position as start position...")
     stepper.setCurrentPosition(stepper_id , 0)
-    sleep(1)
     
     print("Set the motor as engaged...")
     stepper.setEngaged(stepper_id, True)
     sleep(1)
-    
-    print("The motor will run until it reaches the set goal position...")
     
     stepper.setCurrentPosition(stepper_id, 0)
     stepper.setAcceleration(stepper_id, 87543)
@@ -260,15 +276,26 @@ try:
         sensor_dims = 4
 
         all_dim_data = []
+        # will be saved. used to recreate trajectories.
+        coefficients = []
+
+        plt.figure()
+
         for d in range(sensor_dims):
             print('For sensor dimension=' + str(d))
 
             # M = # data points
             # dim_data should be of shape (M,1)
+            # TODO check
             dim_data = []
+
             # of shape (M,)
+            # TODO check
             velocities = []
+
+            # each preset velocity stepper runs at
             for v in range(len(data)):
+                #print('entering outer loop')
                 # group non-empty lists within one velocity
                 trial_data = [x for x in data[v][1] if len(x) > 0]
 
@@ -277,7 +304,24 @@ try:
                 joined = [j for i in trial_data for j in i]
 
                 # select out one dimension for the current regression
-                new_dim_data = [x[d] for x in joined]
+                # TODO occasionally getting index out of range errors
+                # how is that possible?
+                '''
+                min_x_len = min([len(x) for x in joined])
+                print('min_x_len:' + str(min_x_len))
+                for i in range(len(joined)):
+                    if len(x) == min_x_len:
+                        print('in inner loop ' + str(x))
+                print('after loop')
+                '''
+
+                #new_dim_data = [x[d] for x in joined]
+                new_dim_data = []
+                for x in joined:
+                    if len(x) > d:
+                        new_dim_data.append(x[d])
+                    else:
+                        print(str(x) + " wasn't long enough")
 
                 # add a number of repeats of the current velocity
                 # equal to the number of data points we got here
@@ -290,9 +334,13 @@ try:
             # save the nicely formatted data for use manual inspection later
             all_dim_data.append(dim_data)
             
-            plt.figure()
+            # TODO make subplots
+            plt.subplot(2,2,d+1)
             plt.plot(dim_data)
             plt.title('Sensor dimension ' + str(d))
+            # TODO WHAT DETERMINES RATE
+            plt.ylabel('Sensor output along dimension '+str(d))
+            plt.xlabel('Datapoints from sensor')
             plt.show()
         
             # calculate the coefficient relating the ball velocity, 
@@ -300,8 +348,14 @@ try:
             # sensor dimension
             print(np.linalg.lstsq(velocities, \
                 dim_data))
-            X, residuals, rank, singular_values = np.linalg.lstsq(velocities, \
-                dim_data)
+            X, residuals, rank, singular_values = \
+                np.linalg.lstsq(velocities, dim_data)
+
+            # the coefficient relating this sensor dimension
+            # to ball velocity in revolutions per second
+            # assumes X is a scalar
+            print(X.shape)
+            coefficients.append(1 / X)
 
             # TODO test and report on linearity (r^2?)
 
@@ -309,16 +363,44 @@ try:
 
             print(x)
             print(residuals)
+        
+        side = raw_input('Where is the calibration stepper, from ' + \
+            'your perspective? [L/l]eft, [R/r]ight, or [T/t]op.\n' + \
+            'Calibration will not be saved with any other key.\n')
+
+        if side.lower() == 'l':
+            # should cause one dimension of ipsilateral sensor
+            # to vary the most (dim 1 here)
+            with open('left_sensor_calibration.p', 'w') as f:
+                pickle.dump(coefficients, f)
+
+        elif side.lower() == 'r':
+            # should be dim 3 here
+            with open('right_sensor_calibration.p', 'w') as f:
+                pickle.dump(coefficients, f)
+            
+        elif side.lower() == 't':
+            # should vary one dim of each sensor equally (0,2)
+            with open('yaw_sensor_calibration.p', 'w') as f:
+                pickle.dump(coefficients, f)
+
+        else:
+            print('Invalid side. Not saving calibration data.')
     
-# TODO only enclose a small section in this try / except block
 except PhidgetException as e:
     print("Phidget Exception %i: %s" % (e.code, e.details))
 
-try:
+# close serial stuff no matter how we exit
+# with(...) syntax probably the best, but more involved
+finally:
     stepper.setEngaged(0, False)
     sleep(1)
     stepper.closePhidget()
-except PhidgetException as e:
-    print("Phidget Exception %i: %s" % (e.code, e.details))
+    
+    if use_sensor:
+        sensor.close()
+        print('closed connection to sensor')
 
-print("Done.")
+    print('closed connection to stepper motor')
+
+
